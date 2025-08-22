4df30b3d5714334d1d0440a353a1902fd6a99cf7
curbsidenigma <gerardo.domra@gmail.com>
2025-08-22 11:25:16 -0600

    feat(hero): add hero basic content
    Adds a greeting, title and description to the Hero component.
    
    - Sets styles to the Hero component.
    - Introduces a new accent color to be tested.

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -3,7 +3,32 @@ import styles from './Hero.module.scss';
 function Hero() {
   return (
     <>
-      <section className={styles.hero}></section>
+      <section className={styles.hero}>
+        <div className={styles.heroCard}>
+          <div className={styles.heroContent}>
+            <div className={styles.contentWrapper}>
+              <p className={styles.greeting}>
+                <span className={styles.tag}>{'<span>'}</span>
+                Hey, I'm Gerardo
+                <span className={styles.tag}>{'<span>'}</span>
+              </p>
+              <h1>
+                Junior <span className={styles.role}>{'{Full-Stack}'}</span> Web
+                & App Developer
+              </h1>
+              <p className={styles.description}>
+                <span className={styles.tag}>{'<p>'}</span>
+                I'm passionate about clean code. With cutting-edge technologies
+                such as <span className={styles.tag}>NodeJS</span>,{' '}
+                <span className={styles.tag}>React</span>, and{' '}
+                <span className={styles.tag}>Spring Boot</span>... I deliver
+                elegant interfaces and beautiful websites.
+                <span className={styles.tag}>{'<p>'}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
