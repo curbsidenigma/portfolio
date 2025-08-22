b7c72e5710d4dd3f9c5cc8f029800abe8fcb9b1c
curbsidenigma <gerardo.domra@gmail.com>
2025-08-21 22:08:30 -0600

    fix(header): correct behavior when resizing the screen
    Changes sizes and layout when the screen is resized.
    
    - Adds a wrapper that centers the content in the header.
    - Sets a scrollbar when the content is overflowing inside the card.
    - Corrects font, item and image sizes using clamp.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -25,23 +25,25 @@ function Header() {
       <header>
         <div className={styles.headerCard}>
           <div className={styles.headerContent}>
-            <img
-              className={styles.avatar}
-              src={avatarImg}
-              alt="Picture of Gerardo Domínguez Ramírez"
-            />
-            <h2 className={styles.fullName}>
-              <span className={styles.firstName}>Gerardo</span>
-              <span className={styles.lastName}>Domínguez Ramírez</span>
-            </h2>
-            <p className={styles.quote}>
-              I am a junior full-stack developer based in Mexico.
-            </p>
-            <ul className={styles.headerItems}>
-              {headerItems.map(item => (
-                <HeaderItem key={item.id} {...item} />
-              ))}
-            </ul>
+            <div className={styles.contentWrapper}>
+              <img
+                className={styles.avatar}
+                src={avatarImg}
+                alt="Picture of Gerardo Domínguez Ramírez"
+              />
+              <h2 className={styles.fullName}>
+                <span className={styles.firstName}>Gerardo</span>
+                <span className={styles.lastName}>Domínguez Ramírez</span>
+              </h2>
+              <p className={styles.quote}>
+                I am a junior full-stack developer based in Mexico.
+              </p>
+              <ul className={styles.headerItems}>
+                {headerItems.map(item => (
+                  <HeaderItem key={item.id} {...item} />
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </header>
